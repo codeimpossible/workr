c4ceb5b94fe749c4c15318a30ba4a424e6a10872
Jared Barboza <jared.m.barboza@gmail.com>
2017-02-11 15:05:56 -0600

    allow workr to be invoked via cli for testing

--- bin/workr.js
+++ bin/workr.js
@@ -1,10 +1,53 @@
 #!/usr/env node
+'use strict';
 
 const rd = require('require-dir');
-const svcs = rd('./../svcs');
-const jobs = rd('./../jobs');
-const package = require('./../package.json');
+const svcs = rd('./../lib/svcs');
+// const jobs = rd('./../jobs');
+const path = require('path');
+const root = path.resolve(__dirname, '..');
+const pkg = require('./../package.json');
+const yargs = require('yargs');
 
-const out = process.stdout;
+console.log(`Workr v${pkg.version}`);
 
-out(`Workr v${package.version}`);
+// short circuit if --service is passed
+if (~process.argv.indexOf('--service')) {
+  // run service mode
+  return;
+}
+
+const argv = yargs.usage('$0 <cmd> [args]')
+  .command('clone [url]', 'clone a repository locally to the workr temp directory', undefined, (argv) => {
+    argv.job = 'clone';
+  })
+  .command('import [url]', 'imports commits from a cloned reopository', undefined, (argv) => {
+    argv.job = 'import';
+  })
+  .help()
+  .argv;
+
+const blacklist = ['_', '$0', 'command', 'help', 'job'];
+const cleanseArgv = (args) => {
+  let n = {};
+  Object.keys(args).filter(p => !~blacklist.indexOf(p)).forEach(i => {
+    let p = {};
+    p[i] = args[i];
+    n = Object.assign(p, n);
+  });
+  console.log(argv, n);
+  return n;
+};
+
+const Job = require(path.resolve(root, `./lib/jobs/${argv.job}`));
+const JobArgs = require(path.resolve(root, './lib/jobs/job-args'));
+const jobArgs = new JobArgs(cleanseArgv(argv));
+
+const action = new Job({ jobArgs });
+
+action.run().then( result => {
+  console.log('done!');
+})
+.catch( err => {
+  console.error(err.stack);
+});
