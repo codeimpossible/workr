b26e8a7b28277358cfe97440294caedbe508a695
Jared Barboza <jared.m.barboza@gmail.com>
2017-02-11 15:06:34 -0600

    ensure temp directory for test runs

--- specs/setup.js
+++ specs/setup.js
@@ -9,6 +9,8 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
 const mocha = require('mocha');
+const mkdirp = require('mkdirp');
+const path = require('path');
 
 // Add sinon assertions to chai
 chai.use(sinonChai);
@@ -22,6 +24,14 @@ global.fixtures = require('./fixtures');
 global.locator = require('./../lib/service-locator');
 
 global.cleanupRepo = () => new Promise((resolve) => rimraf(config.tmp, resolve));
+global.ensureTempDirectory = () => {
+  return new Promise((resolve, reject) => {
+    mkdirp(path.resolve(__dirname, '../tmp'), (err) => {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
+};
 
 // include our inversion of control setups
 require('./ioc');
